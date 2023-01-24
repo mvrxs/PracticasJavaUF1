a5b9cc991ce7c3219d860c5a1a9c431b6620d8f8
Marcos Díaz Simón <mdazsm@gmail.com>
2023-01-24 21:12:38 +0100

    1.0

--- src/uf1practicas/Pract12.java
+++ src/uf1practicas/Pract12.java
@@ -1,5 +1,6 @@
 package uf1practicas;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Pract12 {
@@ -32,27 +33,12 @@ public class Pract12 {
                 case 1 -> Ex1();
                 case 2 -> Ex2();
                 case 3 -> Ex3();
-//                case 4:
-//                    Ex4();
-//                    break;
-//                case 5:
-//                    Ex5();
-//                    break;
-//                case 6:
-//                    Ex6();
-//                    break;
-//                case 7:
-//                    Ex7();
-//                    break;
-//                case 8:
-//                    Ex8();
-//                    break;
-//                case 9:
-//                    Ex9();
-//                    break;
-//                case 10:
-//                    Ex10();
-//                    break;
+                case 4 -> Ex4();
+                case 5 -> Ex5();
+                case 6 -> Ex6();
+                case 7 -> Ex7();
+                case 8 -> Ex8();
+                case 9 -> Ex9();
                 case 0 -> {
                     System.out.println("Good Bye");
                     System.exit(0);
@@ -150,9 +136,172 @@ public class Pract12 {
                 System.out.println(max + " max");
             } else if (listNum[i] == min) {
                 System.out.println(min + " min");
-            }
-            else System.out.println(listNum[i]);
+            } else System.out.println(listNum[i]);
         }
     }
+
+    static void Ex4() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 04 ----------\n");
+        System.out.println();
+
+        int NUMBERS = 15;
+        int[] listNum = new int[NUMBERS];
+
+        System.out.println("Insert 15 numbers: ");
+
+        for (int i = 0; i < listNum.length; i++) {
+            listNum[i] = sc.nextInt();
+        }
+
+        int num = listNum.length;
+
+        for (int i = listNum.length - 1; i > 0; i--) {
+            listNum[i] = listNum[i - 1];
+
+        }
+
+        listNum[0] = num;
+
+        System.out.println("The numbers are:");
+
+        for (int i = 0; i < listNum.length; i++) {
+            System.out.println(listNum[i]);
+        }
+    }
+
+    static void Ex5() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 05 ----------\n");
+        System.out.println();
+
+        int NUMBER = 100;
+        int[] listNum = new int[NUMBER];
+
+        for (int i = 0; i < listNum.length; i++) {
+            listNum[i] = (int) (Math.random() * 20 + 0);
+            System.out.print(listNum[i] + " ");
+        }
+
+        System.out.print("\n What number do you want to change?");
+        int num1 = sc.nextInt();
+        System.out.print("\n Insert the value to change:");
+        int num2 = sc.nextInt();
+
+        for (int i = 0; i < listNum.length; i++) {
+            if (listNum[i] == num1) {
+                listNum[i] = num2;
+                System.out.print("--->" + listNum[i] + " ");
+            } else System.out.print(listNum[i] + " ");
+        }
+    }
+
+    static void Ex6() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 06 ----------\n");
+        System.out.println();
+
+        int NUMBER = 8;
+        int[] listNum = new int[NUMBER];
+
+        System.out.println("Insert 8 whole numbers: ");
+        for (int i = 0; i < listNum.length; i++) {
+            listNum[i] = sc.nextInt();
+        }
+        for (int i = 0; i < listNum.length; i++) {
+            if (listNum[i] % 2 == 0) {
+                System.out.println(listNum[i] + " par");
+            } else System.out.println(listNum[i] + " inpar");
+        }
+    }
+
+    static void Ex7() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 07 ----------\n");
+        System.out.println();
+
+        int NUMBER = 20;
+        int[] listNum = new int[NUMBER];
+        int j = 0;
+        int pair;
+
+        for (int i = 0; i < listNum.length; i++) {
+            listNum[i] = (int) (Math.random() * 100 + 0);
+            System.out.print(listNum[i] + " ");
+        }
+        System.out.println("\n");
+
+        for (int i = 0; i < listNum.length; i++) {
+            if (listNum[i] % 2 == 0) {
+                pair = listNum[j];
+                listNum[j] = listNum[i];
+                listNum[i] = pair;
+                j++;
+            }
+        }
+        for (int i = 0; i < listNum.length; i++) {
+            System.out.print(listNum[i] + " ");
+        }
+    }
+
+    static void Ex8() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 08 ----------\n");
+        System.out.println();
+
+        int NUMBERS = 10;
+        int[] listNum = new int[NUMBERS];
+        boolean isPrime;
+        int aux = 0;
+        int x = 0;
+        System.out.println("Insert 10 numbers:");
+//ESCRIBIMOS VALORES DEL ARRAY
+        for (int i = 0; i < listNum.length; i++) {
+            //listNum[i] = sc.nextInt();
+            listNum[i] = (int) (Math.random() * 100 + 2);
+        }
+//PRINTAMOS VALORES ESCRITOS DEL ARRAY JUNTO CON SU INDICE
+        for (int i = 0; i < listNum.length; i++) {
+            System.out.println(i + "-->" + listNum[i]);
+        }
+////COMPROBAMOS CUALES SON PRIMOS Y LOS PONEMOS AL INICIO DEL ARRAY
+        for (int i = 0; i < listNum.length; i++) {
+            isPrime = true;
+            for (int j = 2; j * j < listNum[i]; j++) {
+                if (listNum[i] % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                aux = listNum[i];
+                listNum[x] = listNum[i];
+                listNum[i] = aux;
+                x++;
+                System.out.print(listNum[i] + " ");
+            }
+        }
+
+    }
+
+    static void Ex9() {
+
+        System.out.println("\n");
+        System.out.println("---------- EXERCISE 09 ----------\n");
+        System.out.println();
+
+        int NUMBER = 10;
+        int[] listNum = new int[NUMBER];
+
+        System.out.println("Insert 10 numbers:");
+    }
 }
+
+
+
 
